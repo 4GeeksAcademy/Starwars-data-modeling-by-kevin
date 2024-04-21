0f99fea832457d166c6e02fd9070582787489156
KevinGaBo <153501399+KevinGaBo@users.noreply.github.com>
2024-04-21 21:05:31 +0000

    añadidos tablas favoritos

--- src/models.py
+++ src/models.py
@@ -1,29 +1,54 @@
 import os
 import sys
-from sqlalchemy import Column, ForeignKey, Integer, String
+from sqlalchemy import Column, ForeignKey, Integer, String, Boolean
 from sqlalchemy.orm import relationship, declarative_base
 from sqlalchemy import create_engine
 from eralchemy2 import render_er
 
 Base = declarative_base()
 
-class Person(Base):
-    __tablename__ = 'person'
-    # Here we define columns for the table person
-    # Notice that each column is also a normal Python instance attribute.
+class User(Base):
+    __tablename__ = 'user'
     id = Column(Integer, primary_key=True)
-    name = Column(String(250), nullable=False)
+    email = Column(String(120), unique=True, nullable=False)
+    password = Column(String(80), unique=False, nullable=False)
+    is_active = Column(Boolean(), unique=False, nullable=False)
+    fav_planet = relationship('Favorite_planet', back_populates = 'user')
+    fav_character = relationship('Favorite_character', back_populates='user')
 
-class Address(Base):
-    __tablename__ = 'address'
-    # Here we define columns for the table address.
-    # Notice that each column is also a normal Python instance attribute.
+
+class Planet(Base):
+    __tablename__ = 'planet'
     id = Column(Integer, primary_key=True)
-    street_name = Column(String(250))
-    street_number = Column(String(250))
-    post_code = Column(String(250), nullable=False)
-    person_id = Column(Integer, ForeignKey('person.id'))
-    person = relationship(Person)
+    planet_name = Column(String(120), unique=True, nullable=False)
+    population = Column(String(80), unique=False, nullable=False)
+    gravity = Column(String(80), unique=False, nullable=False)
+    fav_planet = relationship('Favorite_planet', back_populates='planet')
+
+
+class Character(Base):
+    __tablename__ = 'character'
+    id = Column(Integer, primary_key=True)
+    character_name = Column(String(120), unique=True, nullable=False)
+    age = Column(String(80), unique=False, nullable=False)
+    weight = Column(String(80), unique=False, nullable=False)
+    fav_character = relationship('Favorite_character', back_populates='character')
+
+class Favorite_planet(Base):
+    __tablename__ = 'favorite_planet'
+    id = Column(Integer, primary_key=True)
+    user_id = Column(Integer, ForeignKey('user.id'))
+    planet_id = Column(Integer, ForeignKey('planet.id'))
+    user = relationship('User', back_populates='fav_planet')
+    planet = relationship('Planet', back_populates='fav_planet')
+
+class Favorite_character(Base):
+    __tablename__ = 'favorite_character'
+    id = Column(Integer, primary_key=True)
+    user_id = Column(Integer, ForeignKey('user.id'))
+    character_id = Column(Integer, ForeignKey('character.id'))
+    user = relationship('User', back_populates='fav_character')
+    character = relationship('Planet', back_populates='fav_character')
 
     def to_dict(self):
         return {}
